0ecdb382873404cccf838792eab22e01760733c5
Michael Gregoire <mdgregoire@gmail.com>
2018-04-09 16:33:34 -0500

    finalizied merge conflict

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -6,13 +6,7 @@ myApp.controller('AdminController', ['UserService', 'NonprofitService','FeedServ
     self.newNonprofit = NonprofitService.newNonprofit;
     self.addNonprofit = NonprofitService.addNonprofit;
     self.getAllNonprofit = NonprofitService.getAllNonprofit;
-
-
-
-<<<<<<< HEAD
-=======
     self.newFeedItem = FeedService.newFeedItem;
     self.addFeedItem = FeedService.addFeedItem;
-  
->>>>>>> 9ec48201d8cfb5ee71dc585322c97ca949a12cce
+
 }]);
