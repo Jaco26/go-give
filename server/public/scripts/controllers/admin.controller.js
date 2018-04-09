e83be2ca2a8486cdfbd62aadb3bcd128c76266fc
Logan Olzenak <logan.olzenak@gmail.com>
2018-04-09 16:15:02 -0500

    feed inputs linked with database

--- server/public/scripts/controllers/admin.controller.js
+++ server/public/scripts/controllers/admin.controller.js
@@ -1,9 +1,12 @@
-myApp.controller('AdminController', ['UserService', 'NonprofitService',/*'AdminService',*/ function(UserService, NonprofitService){
+myApp.controller('AdminController', ['UserService', 'NonprofitService','FeedService',/*'AdminService',*/ function(UserService, NonprofitService, FeedService){
     const self = this;
 
     self.fbLogout = UserService.fbLogout;
 
     self.newNonprofit = NonprofitService.newNonprofit;
     self.addNonprofit = NonprofitService.addNonprofit;
+
+    self.newFeeditem = FeedService.newFeeditem;
+    self.addFeeditem = FeedService.addFeeditem;
   
 }]);
