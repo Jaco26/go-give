c8e06aceeeb14044fdab66bc22dd641631c764f8
Michael Gregoire <mdgregoire@gmail.com>
2018-04-10 16:35:22 -0500

    user discover and feed are displaying stored data

--- server/public/scripts/controllers/user.static.controller.js
+++ server/public/scripts/controllers/user.static.controller.js
@@ -1,8 +1,9 @@
-myApp.controller('UserStaticController', ['UserService', 'FeedService', function(UserService, FeedService){
+myApp.controller('UserStaticController', ['UserService', 'FeedService', 'NonprofitService', function(UserService, FeedService, NonprofitService){
     const self = this;
 
     self.fbLogout = UserService.fbLogout;
     self.user = UserService.user;
 
-    self.allFeedItems = FeedService.allFeedItems
+    self.allFeedItems = FeedService.allFeedItems;
+    self.allNonprofits = NonprofitService.allNonprofits;
 }]);
