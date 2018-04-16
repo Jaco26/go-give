47a3e0d118ee135408f6cd834a98fb97d5e5063f
Selena Carlson <selena.h.carlson@gmail.com>
2018-04-16 15:37:53 -0500

    created daily subscriptions for test

--- server/modules/stripe.create.product.module.js
+++ server/modules/stripe.create.product.module.js
@@ -23,8 +23,8 @@ function createFiveDollarPlan(id, response){
     stripe.plans.create({
         product: id,
         currency: 'usd',
-        interval: 'month',
-        nickname: '$5/month',
+        interval: 'day',
+        nickname: '$5/day',
         amount: 500,
     }, (err, plan) => {
         if(err){
@@ -40,8 +40,8 @@ function createTenDollarPlan(id, response){
     stripe.plans.create({
         product: id,
         currency: 'usd',
-        interval: 'month',
-        nickname: '$10/month',
+        interval: 'day',
+        nickname: '$10/day',
         amount: 1000,
     }, (err, plan) => {
         if(err){
@@ -57,8 +57,8 @@ function createTwentyDollarPlan(id, response){
     stripe.plans.create({
         product: id,
         currency: 'usd',
-        interval: 'month',
-        nickname: '$20/month',
+        interval: 'day',
+        nickname: '$20/day',
         amount: 2000,
     }, (err, plan) => {
         if(err){
