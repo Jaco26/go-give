77c0569f85aabfc0122eb4f9b2408e793ea53082
Michael Gregoire <mdgregoire@gmail.com>
2018-04-27 10:58:28 -0500

    changed server for heroku deploy

--- server/server.js
+++ server/server.js
@@ -18,15 +18,21 @@ app.use(sessionConfig);
 app.use(passport.initialize());
 app.use(passport.session());
 
-//pem generates our SSL Certifiace here
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err
-  }
-//Keys for https are used here
-https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(4430)
-console.log('listening on port 4430');
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function(){
+  console.log(`server listening on port ${PORT}`);
+});//end app listen
+
+// //pem generates our SSL Certifiace here
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//   if (err) {
+//     throw err
+//   }
+// //Keys for https are used here
+// https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(4430)
+// console.log('listening on port 4430');
+// })
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: true}));
